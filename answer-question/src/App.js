84a6e126ec15c1e00f1ca82a11896acf58536dce
ykim8 <ykim879@gatech.edu>
2023-06-05 01:58:05 -0700

    sign in page

--- answer-question/src/App.js
+++ answer-question/src/App.js
@@ -1,26 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+//import { useCollectionData} from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+// private
+});
+
+const auth = firebase.auth();
+//const firestore = firebase.firestore();
+
 
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Is this working?
       </header>
+       <section>
+        {user ? <p>User is here</p> : <SignIn/>}
+      </section> 
     </div>
   );
 }
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sing In With Google</button>
+  )
+}
+
+// function SignOut() {
+//   return auth.currentUser && (
+//     <button onClick={() => auth.signOut()}>Sign Out</button>
+//   )
+// }
+
+// function Discover() {
+
+//   return (
+//     <SignOut></SignOut>
+//   )
+// }
 
 export default App;
