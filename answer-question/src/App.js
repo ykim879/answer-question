a34514f2cc20f5483760eacee17c01e7288e9225
ykim8 <ykim879@gatech.edu>
2023-06-07 00:55:54 -0700

    populated firestore in discoverypage and userprofilepage

--- answer-question/src/App.js
+++ answer-question/src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore'
+
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-//import { useCollectionData} from 'react-firebase-hooks/firestore';
+import { useCollectionData} from 'react-firebase-hooks/firestore';
 
+
+import { useEffect, useState } from 'react';
+
+//import {IonButton} from '@ionic/react';
 
 firebase.initializeApp({
-// private
+  //private data
 });
 
 const auth = firebase.auth();
-//const firestore = firebase.firestore();
+const firestore = firebase.firestore();
 
 
 
@@ -22,11 +28,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        Is this working?
-      </header>
        <section>
-        {user ? <p>User is here</p> : <SignIn/>}
+        {user ? <Discover/> : <SignIn/>}
       </section> 
     </div>
   );
@@ -42,17 +45,88 @@ function SignIn() {
   )
 }
 
-// function SignOut() {
-//   return auth.currentUser && (
-//     <button onClick={() => auth.signOut()}>Sign Out</button>
-//   )
-// }
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
 
-// function Discover() {
+function Discover() {
+  return (
+    <>
+      <UserPage/>
+      <DiscoveryPage/>
+      <SignOut></SignOut>
+      {/*put tab here*/}
+    </>
+  )
+}
 
-//   return (
-//     <SignOut></SignOut>
-//   )
-// }
+function UserPage() {
+  const userRef = firestore.collection('users');
+  const query = userRef.where(firebase.firestore.FieldPath.documentId(), '==', 'user1').limit(1);
+  const [userData] = useCollectionData(query);
 
+  const questionRef = firestore.collection('questions');
+  const questionQuery = questionRef.where(firebase.firestore.FieldPath.documentId(), '==', 'user1').limit(5);
+  const [questionsList] = useCollectionData(questionQuery);
+
+  const answersRef = firestore.collection('answers');
+  const answersQuery = answersRef.where(firebase.firestore.FieldPath.documentId(), '==', 'user1').limit(1);
+  const [answersList] = useCollectionData(answersQuery);
+  return (
+    <>
+    {userData ? "userData is here" + JSON.stringify(userData[0]): "no userData"}
+    <br></br>
+    {questionsList && questionsList.length > 0? JSON.stringify(questionsList) : "there is no question posted by the user"}
+    <br></br>
+    {answersList && answersList.length > 0? JSON.stringify(answersList) : "there is no answers posted by the user"}
+    </>
+  )
+}
+
+
+function DiscoveryPage() {
+  const [postData, setPostData] = useState([]);
+
+  const followingsRef = firestore.collection('followings');
+  const query = followingsRef.doc('user1');
+  const [followingData] = useCollectionData(query);
+
+  //Todo: pagination
+  useEffect(() => {
+    if (followingData) {
+      const fetchPosts = async () => {
+        const postRefs = followingData[0].posts; //followingData[0] is user's followings posts
+
+        const postsDataPromises = postRefs.map(async (postRef) => {
+          const postDoc = await postRef.get();
+
+          if (postDoc.exists) {
+            return postDoc.data();
+          }
+        });
+
+        const postsData = await Promise.all(postsDataPromises);
+        setPostData(postsData);
+      };
+
+      fetchPosts();
+    }
+  }, [followingData]);
+
+  return (
+    <div>
+      <h1>Following Posts</h1>
+      {postData && postData.length > 0? postData.map((post, index) => (
+        <div key={index}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+        </div>
+      )): "no following posts"}
+    </div>
+  );
+}
+
+//create discover page
 export default App;
