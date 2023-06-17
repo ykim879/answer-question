886dd457e8f5cb4ebccb4ac8498f0af85c47c85f
ykim8 <ykim879@gatech.edu>
2023-06-16 20:40:04 -0700

    added content blocks fetching from firestore

--- answer-question/src/ContentBlock.js
+++ answer-question/src/ContentBlock.js
@@ -1,58 +1,86 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from "react";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+import { IonButton } from '@ionic/react';
+
+firebase.initializeApp({
+  //private
+});
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
 
 function ContentBlock() {
   const [contentBlocks, setContentBlocks] = useState([]);
-  const observerRef = useRef(null);
-  const targetRef = useRef(null);
+  const [loadDisable, setLoadDisable] = useState(false);
+  let keys = new Set();
+
+
+  //for pagination
+  let lastVisible = null;
+  // todo: move to service
+  const { email } = auth.currentUser;
+
 
   useEffect(() => {
-    // Create a new IntersectionObserver with a callback function
-    const observer = new IntersectionObserver((entries) => {
-      console.log(entries)
-      const target = entries[0];
-
-      if (target.isIntersecting) {
-        // Load additional content blocks when the target element is intersecting
-        loadContentBlocks();
-      }
-    }, { threshold: 0 });
-
-    // Set the observer reference
-    observerRef.current = observer;
-
-    // Observe the target element
-    if (targetRef.current) {
-      observerRef.current.observe(targetRef.current);
+    if (contentBlocks.length == 0) {
+      loadData()
     }
-
-    // Cleanup the observer when the component is unmounted
-    return () => {
-      if (observerRef.current) {
-        observerRef.current.disconnect();
-      }
-    };
+    console.log("contentBlocks", contentBlocks)
   }, []);
 
-  const loadContentBlocks = () => {
-    // Simulate loading additional content blocks
-    // Replace this with your actual logic to load content blocks
-    const additionalBlocks = [...Array(15)].map((_, index) => ({
-      id: contentBlocks.length + index + 1,
-      text: `Content Block ${contentBlocks.length + index + 1}`
-    }));
+  const loadData = async () => {
+    let questionQuery = firestore
+      .collection("questions")
+      .where("postedUserID", "==", email)
+      .orderBy("timestamp", "desc");
+    if (lastVisible) {
+      questionQuery = questionQuery.startAfter(lastVisible);
+    }
+    questionQuery = questionQuery.limit(5);
 
-    setContentBlocks((prevBlocks) => [...prevBlocks, ...additionalBlocks]);
-  };
+    console.log("query created")
+    questionQuery.get()
+      .then((querySnapshot) => {
+        let newItems = [];
+        querySnapshot.docs.forEach((doc) => {
+          if (!keys.has(doc.id)) {
+            newItems.push({
+              id: doc.id,
+              ...doc.data()
+            })
+            keys.add(doc.id);
+          }
+        });
+        return newItems;
+      })
+      .then((newItems) => {
+        if (newItems) {
+          console.log("newItems ", ...newItems);
+          setContentBlocks((prevBlocks) => [...prevBlocks, ...newItems]);
+          lastVisible = newItems.at(-1);
+          console.log("lastVisible", lastVisible);
+          if (newItems.length < 5) {
+            setLoadDisable(true);
+          }
+        } else {
+          setLoadDisable(true);
+        }
+      })
+  }
 
   return (
-    <div>
+    <div id="contentBlockArea">
       <h1>Content Blocks</h1>
       {contentBlocks.map((block) => (
         <div key={block.id}>
-          <p>{block.text}</p>
+          <p>{block.content}</p>
         </div>
       ))}
-      <div ref={targetRef} />
+      <IonButton disabled={loadDisable} onClick={loadData}>Load More</IonButton>
     </div>
   );
 }
