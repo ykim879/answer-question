b0fedba369381051de505f1f5f1a492999454263
ykim8 <ykim879@gatech.edu>
2023-06-22 13:51:01 -0700

    created UI for user profile

--- answers-questions-tabs/src/pages/Tab2.tsx
+++ answers-questions-tabs/src/pages/Tab2.tsx
@@ -1,18 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonBadge, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+firebase.initializeApp({
+  //private
+});
+
+const firestore = firebase.firestore();
 
 interface ContainerProps {
   email: string;
 }
 
-const UserPage: React.FC<ContainerProps> = ({email}) => {
+const UserPage: React.FC<ContainerProps> = ({ email }) => {
 
   return (
-      <IonContent fullscreen>
-            <IonTitle size="large">{email}</IonTitle>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
+    <IonContent>
+      <div className="profile">
+      <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Name</IonCardTitle>
+            <IonCardSubtitle>{email}</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonLabel>Followers</IonLabel>
+                <IonBadge color="primary">22k</IonBadge>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Followings</IonLabel>
+                <IonBadge color="secondary">22k</IonBadge>
+              </IonItem>
+            </IonList>
+          </IonCardContent>
+
+        </IonCard>
+        </div>
+      <div className="container">
+        
+      </div>
+    </IonContent>
   );
 };
 
