9378b23084a2ef21ca8094973bb0d45058a2eae0
ykim8 <ykim879@gatech.edu>
2023-07-04 19:27:38 -0700

    contentBlock query

--- answers-questions-tabs/src/components/ContentBlockFactory.tsx
+++ answers-questions-tabs/src/components/ContentBlockFactory.tsx
@@ -1,17 +1,53 @@
 //in the contentBlock parameter has reference and by firestore use reference to get data. if the type is question only post question if it is answer with user's answer and question
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/react";
 
-import { IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+firebase.initializeApp({
+    //private
+  });
+  
+  const firestore = firebase.firestore();
+
+interface ContentBlockPageProps {
+    lastVisible: any;
+    email: string;
+}
+
+const ContentBlockPage: React.FC<ContentBlockPageProps> = ({lastVisible, email}) => {
+
+    let questionQuery = firestore
+      .collection("questions")
+      .where("postedUserID", "==", email)
+      .orderBy("timestamp", "desc");
+    if (lastVisible) {
+      questionQuery = questionQuery.startAfter(lastVisible);
+    } //Todo: static variable: lastVisible
+    questionQuery = questionQuery.limit(5);
+
+    const [snapshot, loading, error] = useCollectionOnce(questionQuery,  { idField: 'id' })
+
+    return (
+        <div>
+             {snapshot && snapshot.map(content => <ContentBlockFactory content = {content} />)}
+
+        </div>
+    )
+
+}
 
 //go to page: load all answers of the question -->> need pagination (we need one more content for question-answer relation so that we can get reference and move smoothly to the page)
 interface ContentFactoryProps {
-    reference: string;
+    cotent: Array<string>;
 }
-const ContentBlockFactory: React.FC<ContentFactoryProps> = ({ reference }) => {
+const ContentBlockFactory: React.FC<ContentFactoryProps> = ({ cotent }) => {
     // from firestore get data
     return (
-        <div className="contentBlock">
+        <IonCard>
 
-        </div>
+        </IonCard>
 
     );
 };
@@ -41,4 +77,8 @@ const AnswerBlock: React.FC<AnswerProps> = ({answer}) => {
             {answer}
         </IonCardContent>
     )
+}
+
+function useCollectionOnce(query: any, options: any): [any, any, any] {
+    throw new Error("Function not implemented.");
 }
